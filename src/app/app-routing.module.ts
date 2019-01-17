0e48c11000576d1e142c8099b57cf70a23d60529
Rabel Kaka <Rabelkaka70@gmail.com>
2019-01-17 16:19:00 +1100

    Created fist angular project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //defining the paths in the routes array allows you to connect multiple different components together 
+  //in one page
+  //path is the name you use internally to refer to a specific component
+  //and then you connect this path name to the actual compnonet
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
